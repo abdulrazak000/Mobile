e20d761fccc7aaf6e302fd93b843a57a26281980
abdulrazak000 <asaad76437603@gmail.com>
2026-01-04 02:50:56 -0800

    update db config for railway

--- config.php
+++ config.php
@@ -1,17 +1,17 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 
-$host = getenv("DB_HOST");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASS");
-$db   = getenv("DB_NAME");
-
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli(
+  getenv("MYSQLHOST"),
+  getenv("MYSQLUSER"),
+  getenv("MYSQLPASSWORD"),
+  getenv("MYSQLDATABASE")
+);
 
 if ($conn->connect_error) {
-    echo json_encode([
-        "success" => false,
-        "message" => "Database connection failed"
-    ]);
-    exit;
+  echo json_encode([
+    "success" => false,
+    "message" => "Database connection failed"
+  ]);
+  exit;
 }
